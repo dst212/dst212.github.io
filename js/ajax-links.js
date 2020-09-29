de25fe54d1af8420695acdf2133c1bb2e383a0b7
dst212 <42873548+dst212@users.noreply.github.com>
2020-09-29 16:00:03 +0000

    Added external script support (from fetched page)

--- js/ajax-links.js
+++ js/ajax-links.js
@@ -9,10 +9,10 @@
 var page_onload = () => {};
 
 function fetch_page(name, skip_onload = false, where = document.body.getElementsByTagName('MAIN')[0]) {
-	var file = '/pages/' + name + '.html';
+	var file = '/pages/' + name + ((name[name.length - 1] == '/') ?  'index.html' :  '.html');
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
-		var newpage, parser;
+		var newpage, parser, scripts, script, i;
 		if(this.readyState == 4) {
 			console.log('Fetch of ' + file + ' results: ' + this.status + ' (' + this.statusText + ')');
 			//unfocus the clicked element
@@ -20,16 +20,27 @@ function fetch_page(name, skip_onload = false, where = document.body.getElements
 			//go back on the top of the page
 			window.scrollTo(0, 0);
 			if(this.status == 200) {
-				parser = new DOMParser;
+				parser = new DOMParser();
 				newpage = parser.parseFromString(this.responseText, 'text/html');
 				document.title = newpage.title;
 				document.getElementById('title').innerHTML = newpage.title;
 				where.innerHTML = newpage.body.innerHTML;
-				if(!skip_onload)
+				if(!skip_onload) {
 					page_onload();
+					//add all the javascript scripts from the source page
+					scripts = newpage.getElementsByTagName('SCRIPT');
+					for(i = 0; i < scripts.length; i++) {
+						//just adding the script from the collection won't work, a new script element has to be added
+						script = document.createElement('SCRIPT');
+						script.setAttribute('type', scripts[i].getAttribute('type'));
+						script.setAttribute('src', scripts[i].getAttribute('src'));
+						document.body.appendChild(script);
+						script.innerHTML = scripts[i].innerHTML;
+						script = undefined;
+					}
+				}
 			} else {
-				// fetch_page('/status/' + this.status);
-				document.title = '' + this.status + ' - ' + this.statusText;
+				document.title = '' + this.status + ' - ' + this.statusText ? this.statusText : 'Error';
 				document.getElementById('title').innerHTML = document.title;
 				where.innerHTML = '<div style="display:block; width:100%; text-align: center;"><span style="display:block; font-size: 4em; margin: 0.5em auto;">:(</span><span>The page <i>' + name + '</i> couldn\'t be reached.</span></div>';
 			}
