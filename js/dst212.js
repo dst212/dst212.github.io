d8e9405fdf34beaf62a2ca3e6eabda5c357b32c6
dst212 <42873548+dst212@users.noreply.github.com>
2020-10-18 22:40:56 +0200

    Moved coverPage() and uncoverPage() to dst212.js

--- js/dst212.js
+++ js/dst212.js
@@ -19,4 +19,23 @@ function addFunction(f,t){	//concatenate functions
 	return t ? (f ? () => {f(); t();} : t) : (f ? f : () => {});
 }
 
+function coverPage(color = 'transparent', id = 'cover-page-id') {
+	var div = document.createElement('DIV');
+	div.style.position = 'fixed'; //note: <body> must have absolute or relative position
+	div.style.top = '0';
+	div.style.left = '0';
+	div.style.display = 'block';
+	div.style.height = '100vh';
+	div.style.width = '100vw';
+	div.style.backgroundColor = color;
+	div.style.zIndex = 1; //everything above won't be hidden below
+	div.setAttribute('id', id);
+	document.body.appendChild(div);
+	return div;
+}
+function uncoverPage(id = 'cover-page-id') {
+	var div = document.getElementById(id);
+	if(div)
+		document.body.removeChild(div);
+}
 //END
