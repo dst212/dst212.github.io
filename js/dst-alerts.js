dcbdcd9350b912882c3090cc6c0e08bb805fcca4
dst212 <42873548+dst212@users.noreply.github.com>
2020-10-19 17:08:39 +0200

    Rebuilt notirfications' script (now ntf.js)

--- js/dst-alerts.js
+++ js/dst-alerts.js
@@ -2,30 +2,25 @@
 // https://github.com/dst212/dst212.github.io/
 
 function acceptCookie() {
-	closeNtf('cookies');
 	localStorage.cookie4 = 214;
 }
 function discoModeIncoming() {
-	notify(
-		'disco-mode-incoming',
+	ntf(
 		'WARNING',
 		'By enabling the Disco Mode, one declares to take full resposability for the consequences that may derive from it.',
-		'<button class="normalButton" onclick="closeNtf(\'disco-mode-incoming\');setTheme(\'disco\');">Enable anyway</button> <button class="normalButton" onclick="fetch_page(\'settings/theme\');">More details</button> <button class="normalButton" onclick="closeNtf(\'disco-mode-incoming\');">Cancel</button>',
-		'black',
-		'red'
+		[{innerHTML: 'Enable Anyway', onclick: 'setTheme(\'disco\');', properties: 'class="normalButton"'}, {innerHTML: 'More details', onclick: 'pageFetch(\'settings/theme\');', properties: 'class="normalButton"'}, {innerHTML: 'Cancel', properties: 'class="normalButton"'}],
+		//'<button class="normalButton" onclick="closeNtf(\'disco-mode-incoming\');setTheme(\'disco\');">Enable anyway</button> <button class="normalButton" onclick="pageFetch(\'settings/theme\');">More details</button> <button class="normalButton" onclick="closeNtf(\'disco-mode-incoming\');">Cancel</button>',
+		'black', 'red'
 	);
 }
 
 window.onload = addFunction(window.onload, function() {
 	initNotifications();
 	if(localStorage.cookie4 != 214){
-		notify(
-			'cookies',
+		ntf(
 			'Cookies',
-			"By visiting this website, the use of the mystic and notorious <b>cookies</b> is agreed, <a href=\"https://creativecommons.org/creative-commons-cookies-notice/\" target=\"_blank\">from the gospel of terms and conditions under the Creative Commons license</a>.<br/>Keep calm: they don't bite, but they can hurt other ways.<br/>ACTUALLY no info of users are picked, the websites just stores on the visitors' devices some data about theme and settings, nothing about ad, ID tracking or third party's services.",
-			'<button onclick="acceptCookie();">Agree</button>',
-			'var(--body-fg)',
-			'var(--body-bg)'
+			'By visiting this website, the use of the mystic and notorious <b>cookies</b> is agreed, <a href="https://creativecommons.org/creative-commons-cookies-notice/" target="_blank">from the gospel of terms and conditions under the Creative Commons license</a>.<br/>Keep calm: they don\'t bite, but they can hurt other ways.<br/>ACTUALLY no info of users are picked, the websites just stores on the visitors\' devices some data about theme and settings, nothing about ads, ID tracking or third party\'s services.',
+			[{innerHTML: 'Agree', onclick: 'acceptCookie();'}]//'<button onclick="acceptCookie();">Agree</button>',
 		);
 	}
 });
---
+++ stuff/ntf/ntf.js
@@ -0,0 +1,62 @@
+// made by dst212
+
+/*
+ * This script makes possible to show up a sort of notifications.
+ * Use it with https://dst212.github.io/stuff/notify/notify.css.
+ *
+ * Visit https://github.com/dst212/dst212.github.io to get more details.
+ */
+
+var ntf = (function() {
+	var id = 0;
+	return function(title, content, buttons = [{innerHTML: 'OK', onclick: 'ntfClose(' + (++id) + ')'}], fg = 'var(--body-fg)', bg = 'var(--body-bg)') {
+		var x = document.createElement('DIV');
+		var ntfArea = document.getElementById('notifications');
+		var i, buttonsHTML = '';
+		x.setAttribute('id', 'ntf-' + id);
+		x.style.color = fg;
+		x.style.background = bg;
+		if(buttons.length) {
+			buttonsHTML = '<div class="btn">';
+			for(i = 0; i < buttons.length; i++) {
+				if(!buttons[i].onclick) buttons[i].onclick = 'ntfClose(' + id + ');';
+				else buttons[i].onclick += ';' + 'ntfClose(' + id + ');';
+				buttonsHTML += '<button ';
+				if(buttons[i].properties) buttonsHTML += buttons[i].properties + ' ';
+				buttonsHTML += 'onclick="' + buttons[i].onclick + '">' + buttons[i].innerHTML + '</button>';
+			}
+			buttonsHTML += '</div>';
+		}
+		x.innerHTML = '<small><big class="title">' + title + '</big><div class="msg">' + content + '</div>' + buttonsHTML + '</small>';
+		x.style.display = 'block';
+		ntfArea.append(x);
+		//show up the notification
+		x.style.maxWidth = '100%';
+		x.style.maxHeight = '11em';
+		x.style.opacity = '1';
+		return x;
+	};
+})();
+
+function ntfClose(id) {
+	var x = document.getElementById('ntf-' + id);
+	if(x) {
+		x.style.maxWidth = '0';
+		x.style.maxHeight = '0';
+		x.style.opacity = '0';
+		x.remove();
+	}
+}
+
+function initNotifications() {
+	var x = document.getElementById('notifications');
+	if(!x) {
+		x = document.createElement('DIV');
+		x.setAttribute('id', 'notifications');
+		document.body.appendChild(x);
+		console.log('Notifications\' area appended.');
+	}
+	return x;
+}
+
+//END
