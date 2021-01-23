ab9c378755fb5fd5af680c5695a7c2e178346488
dst212 <42873548+dst212@users.noreply.github.com>
2021-01-23 19:53:08 +0100

    Privacy notice about GitHub Pages added

--- js/dst-alerts.js
+++ js/dst-alerts.js
@@ -17,7 +17,7 @@ window.onload = addFunction(window.onload, function() {
 	if(localStorage.okokItIsFine != 1){
 		ntf(
 			'Welcome',
-			'Welcome to dst212\'s website.<br>Use the navigation bar on top of the screen to access the various pages.<br>By using this website, users agree to save data locally (on their devices) in order to remember the website\'s settings (such as the theme) and other stuff (such as the game data) and blah blah blah this is not a cookie notice blah blah blah nobody cares about this blah blah blah also check my GitHub profile, thanks blah blah blah if someone arrived here the website\'s author apologizes but he congratules on the patience.',
+			'Welcome to dst212\'s website.<br>Use the navigation bar on top of the screen to access the various pages.<br>This website is hosted at <a href="https://github.io" target="_blank" title="GitHub Pages">GitHub Pages</a>. By visiting this website, visitors allow GitHub to collect data from them as mentioned <a href="https://docs.github.com/en/github/site-policy/github-privacy-statement#github-pages" target="_blank">here</a>.<br>By using this website, users agree to save data locally (on their devices) in order to remember the website\'s settings (such as the theme) and other stuff (such as the game data) and blah blah blah this is not a cookie notice blah blah blah nobody cares about this blah blah blah also check my GitHub profile, thanks blah blah blah if someone arrived here the website\'s author apologizes but he congratules on the patience.',
 			[{innerHTML: 'Yes, yes, I don\'t care', onclick: 'okokItIsFine();'}]
 		);
 	}
