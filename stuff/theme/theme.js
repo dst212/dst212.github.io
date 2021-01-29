3de6cab7eee25807201119b157def8cec66733b9
dst212 <42873548+dst212@users.noreply.github.com>
2021-01-29 10:37:03 +0100

    Code restyling and tempMode
    By using set() (or toggle()) methods and setting the "save" parameter to 
    false, data won't be written to localStorage. On calling update(), the 
    theme will be 
    removed and the previous one will be restored.

--- stuff/theme/theme.js
+++ stuff/theme/theme.js
@@ -9,69 +9,97 @@
 
 var Theme = {
 	update: (function () {
-		const darkThemes =	['dark-black-theme',	'dark-white-theme',	'dark-red-theme',	'dark-yellow-theme',	'dark-lime-theme',	'dark-cyan-theme',	'dark-blue-theme',	'dark-magenta-theme'];
-		const lightThemes =	['light-black-theme',	'light-white-theme','light-red-theme',	'light-yellow-theme',	'light-lime-theme',	'light-cyan-theme',	'light-blue-theme',	'light-magenta-theme'];
-		var disco = new Audio(localStorage.discoModeMusic || 'https://dst.altervista.org/files/audios/disco.mp3');
+		const darkThemes =	['dark-red-theme',	'dark-yellow-theme',	'dark-lime-theme',	'dark-cyan-theme',	'dark-blue-theme',	'dark-magenta-theme'];
+		const lightThemes =	['light-red-theme',	'light-yellow-theme',	'light-lime-theme',	'light-cyan-theme',	'light-blue-theme',	'light-magenta-theme'];
+		let tempMode = false, tempModeTheme = localStorage.theme;
+		let disco = new Audio(localStorage.discoModeMusic || 'https://dst.altervista.org/files/audios/disco.mp3');
 		disco.loop = true;
 		function removeClasses() {
 			document.documentElement.classList.forEach((item) => {
-				if(item !== 'white-mode') document.documentElement.classList.remove(item);
+				if(item !== 'white-mode')
+					document.documentElement.classList.remove(item);
 			});
 		}
-		function discoMode(mode, wait, count = 2) {
-			if(localStorage.themeColor === mode) {
+		function discoMode(mode, wait, count = 0) {
+			if(localStorage.themeColor === mode || tempMode) {
 				document.documentElement.classList.remove(darkThemes[count], lightThemes[count++]);
-				if(count >= darkThemes.length) count = 2;
-				if(localStorage.theme === 'white') document.documentElement.classList.add(darkThemes[count]);
-				else if(localStorage.theme === 'black') document.documentElement.classList.add(lightThemes[count]);
-				Theme.computedStyle = undefined; //make it always undefined so that the function getComputedStyle() will be triggered each time the method getComputedStyle() of Theme will be called
+				if(count >= darkThemes.length)
+					count = 0;
+				if(localStorage.theme === 'black' || (tempMode && tempModeTheme === 'black'))
+					document.documentElement.classList.add(lightThemes[count]);
+				else if(localStorage.theme === 'white' || (tempMode && tempModeTheme === 'white'))
+					document.documentElement.classList.add(darkThemes[count]);
+				Theme.computedStyle = null; //make it always null so that the function getComputedStyle() will be triggered each time Theme.getComputedStyle() will be called
 				localStorage.resumeMusic = disco.currentTime;
 				setTimeout(discoMode, wait, mode, wait, count);
 			}
 		}
-		return function() {
+		return function(color, mode) {
+			tempMode = !(!color && !mode); //if both color and mode are not set, tempMode == false
+			if(!color)
+				color = localStorage.themeColor;
+			if(!mode)
+				mode = localStorage.theme;
+			else
+				tempModeTheme = mode;
+
 			removeClasses();
-			if(localStorage.themeColor === 'disco') {
+			if(color === 'disco') {
 				let sleep = Theme.getComputedStyle().getPropertyValue('--duration').slice(0, this.length-1);
-				if(localStorage.playDisco != 1 && localStorage.playDisco != 0) localStorage.playDisco = 1; //will be used for settings in future
+				if(localStorage.playDisco != 1 && localStorage.playDisco != 0)
+					localStorage.playDisco = 1; //will be used for settings in future
 				if(localStorage.playDisco == 1) {
 					disco.currentTime = !localStorage.resumeMusic ? 0 : localStorage.resumeMusic * 1;
 					disco.play();
 				}
 				// console.log('Starting disco mode: ' + sleep + 's delay, ' + disco.currentTime + 's resume');
-				discoMode('disco', (sleep === '') ? 350 : sleep * 1000);
+				discoMode('disco', sleep === '' ? 350 : sleep * 1000);
 			} else {
-				if(!disco.paused) disco.pause();
-				if(localStorage.themeColor === 'quiet') discoMode('quiet', 5000);
-				else if(localStorage.theme === 'white') document.documentElement.classList.add('dark-' + localStorage.themeColor + '-theme');
-				else document.documentElement.classList.add('light-' + localStorage.themeColor + '-theme');
+				if(!disco.paused)
+					disco.pause();
+				if(color === 'quiet')
+					discoMode('quiet', 5000);
+				else {
+					document.documentElement.classList.add((mode === 'white' ? 'dark-' : 'light-') + color + '-theme');
+					if(
+						(mode === 'white' && !document.documentElement.classList.contains('white-mode')) ||
+						(mode !== 'white' && document.documentElement.classList.contains('white-mode'))
+					)
+						document.documentElement.classList.toggle('white-mode');
+				}
 				Theme.computedStyle = getComputedStyle(document.documentElement);
 			}
 		}
 	})(),
-	computedStyle: undefined,
+	computedStyle: null,
 	getComputedStyle: () => (Theme.computedStyle) || (Theme.computedStyle = getComputedStyle(document.documentElement)),
 	background: {
 		get: () => Theme.getComputedStyle().getPropertyValue('--body-fg'),
-		set(id) {
-			if(localStorage.theme != id && (id === 'white' || id === 'black')) {
-				localStorage.theme = id;
-				Theme.update();
+		set(id, save = true) {
+			if((localStorage.theme !== id || !save) && (id === 'white' || id === 'black')) {
+				if(save)
+					localStorage.theme = id;
+				Theme.update(null, id);
 			}
 		},
-		toggle() {
-			if(localStorage.theme === 'white') localStorage.theme = 'black';
-			else localStorage.theme = 'white';
+		toggle(save = true) {
+			if(save) {
+				if(localStorage.theme === 'white')
+					localStorage.theme = 'black';
+				else
+					localStorage.theme = 'white';
+			}
 			document.documentElement.classList.toggle('white-mode');
-			Theme.update();
+			Theme.update(null, document.documentElement.classList.contains('white-mode') ? 'white' : 'black');
 		},
 	},
 	color: {
 		get: () => Theme.getComputedStyle().getPropertyValue('--accent'),
-		set(id) {
-			if(localStorage.themeColor !== id) {
-				localStorage.themeColor = id;
-				Theme.update();
+		set(id, save = true) {
+			if(localStorage.themeColor !== id || !save) {
+				if(save)
+					localStorage.themeColor = id;
+				Theme.update(id);
 			}
 		},
 	},
@@ -79,9 +107,13 @@ var Theme = {
 };
 
 //default theme
-if(localStorage.theme === 'white') document.documentElement.classList.toggle('white-mode');
-else if(localStorage.theme !== 'black') localStorage.theme = 'black';
-if(!localStorage.themeColor) Theme.color.set('lime');
-else Theme.update();
+if(localStorage.theme === 'white')
+	document.documentElement.classList.toggle('white-mode');
+else if(localStorage.theme !== 'black')
+	localStorage.theme = 'black';
+if(!localStorage.themeColor)
+	Theme.color.set('lime');
+else
+	Theme.update();
 
 //END
