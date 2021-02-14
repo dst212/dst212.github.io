9080a815f50e7c6e4a6bd05c3b18f917fce3fe83
dst212 <42873548+dst212@users.noreply.github.com>
2021-02-15 00:11:08 +0100

    Project restyle
    - PC keyboard replaced with a piano keyboard.
    - Octaves added.

--- pages/stuff/beep/beep.js
+++ pages/stuff/beep/beep.js
@@ -9,8 +9,6 @@
 			//should've been something similar to a piano sound, but I'm not into sound waves
 			'piano': ctx.createPeriodicWave(new Float32Array([0, Math.sqrt(3)/2, 0, 0]), new Float32Array([0, 0.25, 0, -0.25])),
 		};
-		for(let key in keys) if(keys[key] !== 'br')
-			keys[key].osc = oscillator(keys[key].value * 1);
 	}
 
 	//they're not constant because can be modified in future
@@ -38,31 +36,32 @@
 		};
 		return out || '';
 	}
+	const isBlack = note => noteToString(note)[1] === '#'; //is a black key
 
-	let keyboard = document.getElementById('beep-keyboard'); //div containing keys
-	let offset = -11; //the first note in assign[]. 0 is A (La)
-	let assign = ['KeyA', ['KeyZ', ['KeyS']], 'KeyX', 'KeyD', 'KeyC', 'KeyF', ['KeyV', ['KeyG']], 'KeyB', 'KeyH', 'KeyN', ['KeyJ', 'Digit1'], ['KeyM', 'KeyQ'], ['KeyK', 'Digit2'], ['Comma', 'KeyW', ['KeyL', 'Digit3']], ['Period', 'KeyE'], ['Semicolon', 'Digit4'], ['Slash', 'KeyR'], ['Quote', 'Digit5'], ['KeyT', ['Digit6']], 'KeyY', 'Digit7', 'KeyU', 'Digit8', 'KeyI', 'Digit9', ['KeyO', ['Digit0']], 'KeyP', 'Minus', 'BracketLeft', 'Equal', ['BracketRight', ['Backspace']]];
-	//keyboard layout object
-	let keys = {
-		'a': 'br', 'Digit1': null, 'Digit2': null, 'Digit3': null, 'Digit4': null, 'Digit5': null, 'Digit6': null, 'Digit7': null, 'Digit8': null, 'Digit9': null, 'Digit0': null,    'Minus': null, 'Equal': null, 'Backspace': null,
-		'b': 'br', 'KeyQ': null,   'KeyW': null,   'KeyE': null,   'KeyR': null,   'KeyT': null,   'KeyY': null,   'KeyU': null,   'KeyI': null,   'KeyO': null,   'KeyP': null,      'BracketLeft': null,'BracketRight': null,
-		'c': 'br', 'KeyA': null,   'KeyS': null,   'KeyD': null,   'KeyF': null,   'KeyG': null,   'KeyH': null,   'KeyJ': null,   'KeyK': null,   'KeyL': null,   'Semicolon': null, 'Quote': null,
-		'd': 'br', 'KeyZ': null,   'KeyX': null,   'KeyC': null,   'KeyV': null,   'KeyB': null,   'KeyN': null,   'KeyM': null,   'Comma': null,  'Period': null, 'Slash': null,
-		'e': 'br'
-	};
-	//assign each note to the respective key
-	for(let i = 0; i < assign.length; i++) {
-		if(typeof assign[i] === 'object') {
-			for(let j = 0; j < assign[i].length; j++) {
-				if(typeof assign[i][j] === 'object') {
-					for(let k = 0; k < assign[i][j].length; k++) //duplicated keys
-						keys[assign[i][j][k]] = (i + offset).toString();
-				} else
-					keys[assign[i][j]] = i + offset;
-			}
-		} else
-			keys[assign[i]] = i + offset;
+	//symbol of a given key code
+	function symbolOf(key) {
+		let out;
+		switch(key) {
+			default: out = key.slice(-1); break;
+			case 'Minus': out = '-'; break;
+			case 'Equal': out = '='; break;
+			case 'Backspace': out = '←'; break;
+			case 'BracketLeft': out = '['; break;
+			case 'BracketRight': out = ']'; break;
+			case 'Semicolon': out = ';'; break;
+			case 'Quote': out = '\''; break;
+			case 'Comma': out = ','; break;
+			case 'Period': out = '.'; break;
+			case 'Slash': out = '/'; break;
+			case 'IntlBackslash': out = '\\'; break;
+		}
+		return out || '';
 	}
+
+	const keyboard = document.getElementById('beep-keyboard'); //div containing piano keys
+	let offset = -12; //the first note in assign[]. 0 is A (La)
+	let assign = [['IntlBackslash'], ['KeyA'], ['KeyZ', 'KeyS'], ['KeyX'], ['KeyD'], ['KeyC'], ['KeyF'], ['KeyV', 'KeyG'], ['KeyB'], ['KeyH'], ['KeyN'], ['KeyJ', 'Digit1'], ['KeyM', 'KeyQ'], ['KeyK', 'Digit2'], ['KeyW', 'KeyL', 'Comma', 'Digit3'], ['KeyE', 'Period'], ['Digit4', 'Semicolon'], ['KeyR', 'Slash'], ['Digit5', 'Quote'], ['KeyT', 'Digit6'], ['KeyY'], ['Digit7'], ['KeyU'], ['Digit8'], ['KeyI'], ['Digit9'], ['KeyO', 'Digit0'], ['KeyP'], ['Minus'], ['BracketLeft'], ['Equal'], ['BracketRight', 'Backspace']];
+	const keys = {}; //keys with respective buttons
 
 	//oscillators and their object
 	let oscillators = {};
@@ -76,97 +75,151 @@
 		return osc;
 	}
 
-	//"light up" the keys
-	function turnOn(value) {
-		let i = value - offset;
-		if(typeof assign[i] === 'object') {
-			for(let j = 0; j < assign[i].length; j++)
-				if(typeof assign[i][j] === 'string')
-					keys[assign[i][j]].classList.add('beep-active');
-		} else
-			keys[assign[i]].classList.add('beep-active');
-	}
-	function turnOff(value) {
-		let i = value - offset;
-		if(typeof assign[i] === 'object') {
-			for(let j = 0; j < assign[i].length; j++)
-				if(typeof assign[i][j] === 'string')
-					keys[assign[i][j]].classList.remove('beep-active');
-		} else
-			keys[assign[i]].classList.remove('beep-active');
-	}
-
-	//create keys from object and append to keyboard
-	for(let key in keys) {
-		let el, value = keys[key]; //value has to be local
-		if(value === 'br') {
-			el = document.createElement('BR');
-		} else {
-			el = document.createElement('BUTTON');
-			el.id = 'beep-' + key;
-			switch(key) {
-				default: el.innerHTML = key.slice(-1); break;
-				case 'Minus': el.innerHTML = '-'; break;
-				case 'Equal': el.innerHTML = '='; break;
-				case 'Backspace': el.innerHTML = '←'; break;
-				case 'BracketLeft': el.innerHTML = '['; break;
-				case 'BracketRight': el.innerHTML = ']'; break;
-				case 'Semicolon': el.innerHTML = ';'; break;
-				case 'Quote': el.innerHTML = '\''; break;
-				case 'Comma': el.innerHTML = ','; break;
-				case 'Period': el.innerHTML = '.'; break;
-				case 'Slash': el.innerHTML = '/'; break;
-			}
-			if(typeof value === 'string') //duplicated button
-				value *= 1, el.style.opacity = '0.5';
-			if(typeof value === 'number') { //if value is a number, then it's a valid note
-				let lower, upper;
-				if((el.value = value) === 0)
-					el.classList.add('invalid', 'btn');
-				el.active = false;
-				el.onmousedown = function(e) {
+	(function() {
+		function appendKey(i) {
+			let value = i + offset, el = document.createElement('BUTTON');
+			el.value = value;
+			if(isBlack(value))
+				el.classList.add('beep-black');
+			el.ontouchstart = el.onmouseover = el.onmousedown = function(e) {
+				if(!ctx)
+					init();
+				if((e.buttons > 0 && e.button === 0) || (e.code) && !this.active) {
+					this.classList.add('beep-active');
+					this.osc = oscillator(value + shift.value() * 12);
 					this.active = true;
-					if(e.shiftKey) {
-						lower = oscillator(value - 12);
-						lower.type = TYPE;
-						lower.connect(ctx.destination);
-					}
 					if(customWaves[TYPE])
 						this.osc.setPeriodicWave(customWaves[TYPE]);
 					else
 						this.osc.type = TYPE;
 					this.osc.connect(ctx.destination);
-					turnOn(value);
 					noteDiv.innerHTML = noteToString(value);
 				}
-				el.onmouseup = function(e) {
-					lower?.disconnect();
+			}
+			el.ontouchcancel = el.ontouchend = el.onmouseout = el.onmouseup = function(e) {
+				if(this.active) {
+					this.classList.remove('beep-active');
 					this.osc.disconnect();
 					this.active = false;
-					turnOff(value);
 					if(noteDiv.innerHTML === noteToString(value))
 						noteDiv.innerHTML = '';
 				}
-			} else { //if value is null, it's not a clickable button
-				el.setAttribute('disabled', '');
 			}
-			//replacing the old value with the button reference (to reach them quickly)
-			keys[key] = el;
+			return keyboard.appendChild(el);
 		}
-		keyboard.appendChild(el);
+
+		//assign each note to the respective key
+		for(let i = 0; i < assign.length; i++) {
+			let el = appendKey(i);
+			el.innerHTML = symbolOf(assign[i][0]);
+			for(let j = 0; j < assign[i].length; j++)
+				keys[assign[i][j]] = el;
+		}
+	})();
+
+	//octave shift (1 == 12 semitones above, -1 == 12 semitones below)
+	const shift = (function() {
+		let s = 0;
+		const min = -2, max = 3;
+		const div = document.getElementById('beep-octave');
+		const clearActive = () => div.childNodes.forEach(item => item.classList.remove('beep-active'));
+		for(let i = min; i <= max; i++) {
+			let cur = i, btn = document.createElement('BUTTON');
+			btn.innerHTML = cur + 4;
+			if(cur === 0)
+				btn.classList.add('beep-active');
+			btn.classList.add('margin', 'square');
+			btn.onclick = function(e) {
+				s = cur;
+				clearActive();
+				this.classList.add('beep-active');
+			};
+			div.appendChild(btn);
+		}
+		return {
+			value: () => s,
+			min: () => min,
+			max: () => max,
+			left() {
+				if(s > min) {
+					--s;
+					clearActive();
+					div.childNodes[s - min].classList.add('beep-active');
+				}
+			},
+			right() {
+				if(s < max) {
+					++s;
+					clearActive();
+					div.childNodes[s - min].classList.add('beep-active');
+				}
+			},
+			reset() {
+				s = 0;
+				clearActive();
+				div.childNodes[s - min].classList.add('beep-active');
+			},
+		};
+	})();
+
+	//key action check
+	function pressed(e) {
+		switch(e.code) {
+			case 'ArrowLeft': shift.left(); break;
+			case 'ArrowRight': shift.right(); break;
+			case 'Space': shift.reset(); break;
+			default:
+				if(keys[e.code])
+					keys[e.code].onmousedown(e);
+				else
+					return false;
+		}
+		return true;
+	}
+	function released(e) {
+		if(keys[e.code]) {
+			keys[e.code].onmouseup(e);
+			return true;
+		}
 	}
 
+	//events
+	(function() {
+		let onkeyup = document.onkeyup;
+		let onkeydown = document.onkeydown;
+		document.onkeydown = function(e) {
+			if(
+				document.activeElement.nodeName !== 'INPUT' &&
+				document.activeElement.nodeName !== 'TEXTAREA' &&
+				pressed(e)
+			)
+				return false;
+		};
+		document.onkeyup = function(e) {
+			if(
+				document.activeElement.nodeName !== 'INPUT' &&
+				document.activeElement.nodeName !== 'TEXTAREA' &&
+				released(e)
+			)
+				return false;
+		};
+		//restore old events when the page gets unloaded
+		Page.unload = function() {
+			document.onkeyup = onkeyup;
+			document.onkeydown = onkeydown;
+		};
+	})();
+
+	//settings popup
 	let settingsPopup = new Popup('Beep - Settings', `
-		<div class="radio">
-			Wave type:
-		</div>
+		<div class="radio margin-bottom">Wave type:</div><br>
+		<div class="checkbox margin-bottom"></div>
 	`, [{innerHTML: 'Ok'}], {draggable: true});
 
 	//print a radio input for each wave type
 	(function() {
-		let radio = settingsPopup.content.getElementsByClassName('radio')[0];
+		let label, input, div = settingsPopup.content.getElementsByClassName('radio')[0];
 		['sine', 'square', 'sawtooth', 'triangle'].forEach((item, i) => {
-			let label, input;
 			label = document.createElement('LABEL');
 			label.innerHTML = item[0].toUpperCase() + item.slice(1);
 			input = document.createElement('INPUT');
@@ -181,52 +234,43 @@
 				input.setAttribute('checked', '');
 			label.appendChild(input);
 			label.appendChild(document.createElement('SPAN'));
-			radio.appendChild(label);
+			div.appendChild(label);
 		});
+
+		//hide key references on keyboard's keys and hints above the keyboard
+		const hide = () =>  document.getElementById('beep-hints').style.maxHeight = keyboard.style.fontSize = '0px';
+		const show = () =>  document.getElementById('beep-hints').style.maxHeight = keyboard.style.fontSize = '';
+		div = settingsPopup.content.getElementsByClassName('checkbox')[0];
+		label = document.createElement('LABEL');
+		label.innerHTML = 'Hide hints';
+		input = document.createElement('INPUT');
+		input.type = 'checkbox';
+		input.onchange = function(e) {
+			this.blur();
+			if(this.checked) {
+				localStorage.setItem('beep-clean-keys', '1');
+				hide();
+			} else {
+				localStorage.removeItem('beep-clean-keys');
+				show();
+			}
+		}
+		if(localStorage.getItem('beep-clean-keys')) {
+			hide();
+			input.setAttribute('checked', '');
+		}
+		label.appendChild(input);
+		label.appendChild(document.createElement('SPAN'));
+		div.appendChild(label);
 	})();
 
 	//settings button
 	(function() {
 		let btn = document.createElement('BUTTON');
 		btn.innerHTML = 'settings';
-		btn.classList.add('material-icons', 'md-same');
+		btn.classList.add('material-icons', 'md-same', 'square', 'margin');
 		btn.onclick = e => settingsPopup.open();
-		keyboard.appendChild(btn);
-	})();
-
-	//events
-	(function() {
-		let onkeyup = document.onkeyup;
-		let onkeydown = document.onkeydown;
-		let onmousedown = document.onmousedown;
-		document.onmousedown = function(e) {
-			init();
-			(document.onmousedown = onmousedown)?.();
-		}
-		//trigger keys when they are pressed
-		document.onkeydown = function(e) {
-			if(!ctx)
-				document.onmousedown(e);
-			if(
-				document.activeElement.nodeName !== 'INPUT' &&
-				document.activeElement.nodeName !== 'TEXTAREA' &&
-				keys[e.code]?.active === false
-			) {
-				keys[e.code].onmousedown(e);
-				return false;
-			}
-		};
-		document.onkeyup = function(e) {
-			if(keys[e.code]?.active) {
-				keys[e.code].onmouseup(e);
-				return false;
-			}
-		};
-		//restore old events when the page gets unloaded
-		Page.unload = function() {
-			document.onkeyup = onkeyup;
-			document.onkeydown = onkeydown;
-		};
+		noteDiv.parentNode.insertBefore(btn, noteDiv);
 	})();
 })();
 
